443202ee43470b7995ad420e12baa6a03fa3e27b
Anna Volodkova <kvetka99@mail.ru>
2020-08-13 12:54:11 +0300

    Add id to book on server side

--- src/resources/writer/books-update/index.js
+++ src/resources/writer/books-update/index.js
@@ -1,9 +1,19 @@
+const uid = require('uid');
+
 const writerService = require('../writer.service');
 
 async function handler(ctx) {
   const {
     firstName, lastName, age, books,
   } = ctx.request.body;
+
+  books.map((item) => {
+    return {
+      ...item,
+      _id: uid(),
+    };
+  });
+
   ctx.body = await writerService._collection.update(
     { _id: ctx.params.id },
     {
--- src/resources/writer/create-writer/index.js
+++ src/resources/writer/create-writer/index.js
@@ -1,9 +1,19 @@
+const uid = require('uid');
+
 const writerService = require('../writer.service');
 
 async function handler(ctx) {
   const {
     firstName, lastName, age, books,
   } = ctx.request.body;
+
+  books.map((item) => {
+    return {
+      ...item,
+      _id: uid(),
+    };
+  });
+
   await writerService.create({
     firstName, lastName, age, books,
   });
